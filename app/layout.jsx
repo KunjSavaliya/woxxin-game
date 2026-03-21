00b9a6edd9f1a119eb62f079a5d2d0c1b41bd71d
KunjSavaliya <kunj.bvminfotech@gmail.com>
2026-03-21 11:07:51 +0530

    update project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,28 @@
 import Footer from "./components/common/footer";
 import Navbar from "./components/common/navbar";
 import "./globals.css";
-import { Wendy_One } from "next/font/google";
+import { Wendy_One, Poppins } from "next/font/google";
 
 export const metadata = {
   title: "Woxxin Games | Next-Generation Gaming Experiences",
   description: "Crafting immersive, high-performance games designed to entertain, engage, and deliver unforgettable digital adventures.",
 };
 
-const wendyOne = Wendy_One({
+const wendy = Wendy_One({
   subsets: ["latin"],
   weight: "400",
+  variable: "--font-wendy",
+});
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={wendyOne.className}>
+      <body className={`${wendy.variable} ${poppins.variable}`}>
         <Navbar />
         {children}
         <Footer />
