101bb75ad890431f25039dd0b7db1a0d5a573172
KunjSavaliya <kunj.bvminfotech@gmail.com>
2026-03-21 15:04:07 +0530

    update project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,45 @@
+// import CustomCursor from "./components/common/CustomCursor";
+// import Footer from "./components/common/footer";
+// import Navbar from "./components/common/navbar";
+// import RouteLoader from "./components/common/RouteLoader";
+// import SmoothScrollProvider from "./components/common/SmoothScrollProvider";
+
+// import "./globals.css";
+// import { Wendy_One, Poppins } from "next/font/google";
+
+// export const metadata = {
+//   title: "Woxxin Games | Next-Generation Gaming Experiences",
+//   description: "Crafting immersive, high-performance games designed to entertain, engage, and deliver unforgettable digital adventures.",
+// };
+
+// const wendy = Wendy_One({
+//   subsets: ["latin"],
+//   weight: "400",
+//   variable: "--font-wendy",
+// });
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"],
+//   variable: "--font-poppins",
+// });
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${wendy.variable} ${poppins.variable}`}>
+//         <RouteLoader>
+//           <CustomCursor />
+//           <Navbar />
+//           <SmoothScrollProvider>
+//             {children}
+//           </SmoothScrollProvider>
+//           <Footer />
+//         </RouteLoader>
+//       </body>
+//     </html>
+//   );
+// }
+
 import CustomCursor from "./components/common/CustomCursor";
 import Footer from "./components/common/footer";
 import Navbar from "./components/common/navbar";
@@ -5,28 +47,24 @@ import RouteLoader from "./components/common/RouteLoader";
 import SmoothScrollProvider from "./components/common/SmoothScrollProvider";
 
 import "./globals.css";
-import { Wendy_One, Poppins } from "next/font/google";
+import { Share } from "next/font/google";
 
 export const metadata = {
   title: "Woxxin Games | Next-Generation Gaming Experiences",
-  description: "Crafting immersive, high-performance games designed to entertain, engage, and deliver unforgettable digital adventures.",
+  description:
+    "Crafting immersive, high-performance games designed to entertain, engage, and deliver unforgettable digital adventures.",
 };
 
-const wendy = Wendy_One({
+const share = Share({
   subsets: ["latin"],
   weight: "400",
-  variable: "--font-wendy",
-});
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
+  variable: "--font-share",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${wendy.variable} ${poppins.variable}`}>
+      <body className={share.variable}>
         <RouteLoader>
           <CustomCursor />
           <Navbar />
